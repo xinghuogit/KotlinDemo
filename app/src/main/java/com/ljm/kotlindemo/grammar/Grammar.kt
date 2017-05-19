f4d3a94a43fe6a2755fe4b92201e2b370e845be9
李加蒙 <1605651971@qq.com>
2017-05-19 19:15:18 +0800

    ceshi

---
+++ app/src/main/java/com/ljm/kotlindemo/grammar/Grammar.kt
@@ -0,0 +1,37 @@
+package com.ljm.kotlindemo.grammar
+
+/**
+ * 作者：LIJM on 2017/5/19 18:58
+ * 邮箱：1822396822@qq.com
+ * 描述：
+ * Version:1.0
+ */
+/**
+ * 带有两个 Int 参数、返回 Int 的函数：
+ */
+//fun sum(a: Int, b: Int): Int {
+//    return a + b
+//}
+
+/**
+ * 将表达式作为函数体、返回值类型自动推断的函数：
+ */
+//fun sum(a: Int, b: Int) = a + b
+
+/**
+ * 函数返回无意义的值：
+ */
+//fun printSum(a: Int, b: Int): Unit {
+//    println("sum of $a + $b = ${a + b}")
+//}
+
+fun main(args: Array<String>) {
+
+//    print("3 + 5 = ")
+//    println(sum(3, 5))
+
+//    println("14 + 15 = ${sum(14, 15)}")
+
+//    printSum(-2, 9)
+}
+
---
+++ app/src/main/java/com/ljm/kotlindemo/grammar/GrammarActivity.kt
@@ -0,0 +1,13 @@
+package com.ljm.kotlindemo.grammar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ljm.kotlindemo.R
+
+class GrammarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grammar)
+    }
+}
