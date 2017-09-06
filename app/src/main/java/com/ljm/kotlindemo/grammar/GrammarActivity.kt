c34e9c053a2f2d27593dd1d8be7a1a33597481d8
李加蒙 <1605651971@qq.com>
2017-09-06 16:13:39 +0800

    ceshi

--- app/src/main/java/com/ljm/kotlindemo/grammar/GrammarActivity.kt
+++ app/src/main/java/com/ljm/kotlindemo/grammar/GrammarActivity.kt
@@ -2,12 +2,16 @@ package com.ljm.kotlindemo.grammar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ljm.kotlindemo.R
 
-class GrammarActivity : AppCompatActivity() {
-
+class GrammarActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grammar)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
